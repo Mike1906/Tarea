5a45616ba3bc5158689c33563164017911d7cc34
maur1c10 <xmaur1c10x@hotmail.com>
2014-11-06 21:39:54 -0600

    agregados daos y controladores
    agregados daos y controladores de JRV y Padron electoral

---
+++ src/negocio/CtrlPadronElectoral.java
@@ -0,0 +1,48 @@
+package negocio;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import dao.PadronElectoralDAO;
+import dominio.PadronElectoral;
+
+
+public class CtrlPadronElectoral {
+	 private PadronElectoralDAO daoPadron = new PadronElectoralDAO() ;  
+     
+     public boolean crearPadron(String dui, String nombre, String apellidos,
+    		 String domicilio,String estadovotacion,int idurna, String username,Date fechanac) { 
+        if (daoPadron.daPadronByDui(dui) == null ) { 
+         PadronElectoral padron = new PadronElectoral(dui,nombre,apellidos,domicilio,estadovotacion,idurna,username,fechanac); 
+              daoPadron.guardaActualiza(padron) ; 
+              return true;
+        } 
+        else 
+          return false ; 
+     } 
+      
+     public boolean borrarPadron(String dui) { 
+       if (daoPadron.daPadronByDui(dui) != null ) { 
+    	   PadronElectoral padron = daoPadron.daPadronByDui(dui) ; 
+    	   daoPadron.eliminar(padron) ; 
+         return true ; 
+       } 
+       else  
+         return false ; 
+     } 
+      
+     
+     
+     public List<PadronElectoral> daPadrones(){ 
+         return daoPadron.daPadron() ; 
+     } 
+
+     public PadronElectoral daPadronByDui(String dui) { 
+          return daoPadron.daPadronByDui(dui)  ; 
+     } 
+      
+    /* public PadronElectoral daPadronByUrna(String id_urna){ 
+          return daoPadron.daPadronByUrna(id_urna); 
+     }*/
+}
