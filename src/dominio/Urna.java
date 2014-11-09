32e0c52668ab29c1d264be1a6e2079c9a94a9da8
Portillo <Portillo@Portillo-PC>
2014-11-08 22:06:48 -0600

    atributos arreglados

--- src/dominio/Urna.java
+++ src/dominio/Urna.java
@@ -16,186 +16,138 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "urna", catalog = "tarea", schema = "")
 @NamedQueries({
-@NamedQuery(name = "Urna.findAll", query = "SELECT d FROM Urna d"),
-@NamedQuery(name = "Urna.findByIdUrna", query = "SELECT d FROM Urna d WHERE d.idUrna = :idUrna"),
+@NamedQuery(name = "Urna.findAll", query = "SELECT u FROM Urna u"),
+@NamedQuery(name = "Urna.findByIdUrna", query = "SELECT u FROM Urna u WHERE d.idUrna = :idUrna"),
 })
 
 
 public class Urna implements Serializable {
 
-	private int idUrna;
-	private int idMunicipio;
-	private int idDepartamento;
-	private int numJunta;
-	private String presidente;
-	private String secretario;
-	private int cantVotosNulos;
-	private int cantVotosNoValidos;
-	private String userName;
-
-
-
-
+	private int idurna;  
+	private int idmunicipio;  
+	private int iddepto;  
+	private int numjunta;  
+	private String presidente;  
+	private String secretario;  
+	private int cantvotosnulos;  
+	private int cantvotosnovalid;  
+	private int username;
 	public Urna() {
-
-		
-	}
-
-
-
-
-	public Urna(int idUrna, int idMunicipio, int idDepartamento, int numJunta,
-			String presidente, String secretario, int cantVotosNulos,
-			int cantVotosNoValidos, String userName) {
 		super();
-		this.idUrna = idUrna;
-		this.idMunicipio = idMunicipio;
-		this.idDepartamento = idDepartamento;
-		this.numJunta = numJunta;
+		// TODO Auto-generated constructor stub
+	}
+	public Urna(int idurna, int idmunicipio, int iddepto, int numjunta,
+			String presidente, String secretario, int cantvotosnulos,
+			int cantvotosnovalid, int username) {
+		super();
+		this.idurna = idurna;
+		this.idmunicipio = idmunicipio;
+		this.iddepto = iddepto;
+		this.numjunta = numjunta;
 		this.presidente = presidente;
 		this.secretario = secretario;
-		this.cantVotosNulos = cantVotosNulos;
-		this.cantVotosNoValidos = cantVotosNoValidos;
-		this.userName = userName;
+		this.cantvotosnulos = cantvotosnulos;
+		this.cantvotosnovalid = cantvotosnovalid;
+		this.username = username;
 	}
-
-
-
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Basic(optional = false)
-	@Column(name = "IdUrna")
-	public int getIdUrna() {
-		return idUrna;
+	@Column(name = "idurna")
+	public int getIdurna() {
+		return idurna;
 	}
-
-
-
-
-	public void setIdUrna(int idUrna) {
-		this.idUrna = idUrna;
+	
+	
+	public void setIdurna(int idurna) {
+		this.idurna = idurna;
 	}
-
-
-
+	
 	@Basic(optional = false)
-	@Column(name = "IdMunicipio")
-	public int getIdMunicipio() {
-		return idMunicipio;
+	@Column(name = "idmunicipio")
+	public int getIdmunicipio() {
+		return idmunicipio;
 	}
-
-
-
-
-	public void setIdMunicipio(int idMunicipio) {
-		this.idMunicipio = idMunicipio;
+	
+	public void setIdmunicipio(int idmunicipio) {
+		this.idmunicipio = idmunicipio;
 	}
-
-
-
+	
 	@Basic(optional = false)
-	@Column(name = "IdDepartamento")
-	public int getIdDepartamento() {
-		return idDepartamento;
+	@Column(name = "iddepto")
+	public int getIddepto() {
+		return iddepto;
 	}
-
-
-
-
-	public void setIdDepartamento(int idDepartamento) {
-		this.idDepartamento = idDepartamento;
+	
+	public void setIddepto(int iddepto) {
+		this.iddepto = iddepto;
 	}
-
-
-
+	
+	
 	@Basic(optional = false)
-	@Column(name = "NumJunta")
-	public int getNumJunta() {
-		return numJunta;
+	@Column(name = "numjunta")
+	public int getNumjunta() {
+		return numjunta;
 	}
-
-
-
-
-	public void setNumJunta(int numJunta) {
-		this.numJunta = numJunta;
+	
+	public void setNumjunta(int numjunta) {
+		this.numjunta = numjunta;
 	}
-
-
-
+	
+	
 	@Basic(optional = false)
-	@Column(name = "Presidente")
+	@Column(name = "presidente")
 	public String getPresidente() {
 		return presidente;
 	}
-
-
-
-
+	
 	public void setPresidente(String presidente) {
 		this.presidente = presidente;
 	}
-
-
-
+	
+	
 	@Basic(optional = false)
-	@Column(name = "Secretario")
+	@Column(name = "secretario")
 	public String getSecretario() {
 		return secretario;
 	}
-
-
-
-
+	
 	public void setSecretario(String secretario) {
 		this.secretario = secretario;
 	}
-
-
-
+	
+	
 	@Basic(optional = false)
-	@Column(name = "CantVotosNulos")
-	public int getCantVotosNulos() {
-		return cantVotosNulos;
+	@Column(name = "cantvotosnulos")
+	public int getCantvotosnulos() {
+		return cantvotosnulos;
 	}
-
-
-
-
-	public void setCantVotosNulos(int cantVotosNulos) {
-		this.cantVotosNulos = cantVotosNulos;
+	
+	public void setCantvotosnulos(int cantvotosnulos) {
+		this.cantvotosnulos = cantvotosnulos;
 	}
-
-
-
+	
+	
 	@Basic(optional = false)
-	@Column(name = "CantVotosValidos")
-	public int getCantVotosNoValidos() {
-		return cantVotosNoValidos;
+	@Column(name = "cantvotosnovalid")
+	public int getCantvotosnovalid() {
+		return cantvotosnovalid;
 	}
-
-
-//
-	public void setCantVotosNoValidos(int cantVotosNoValidos) {
-		this.cantVotosNoValidos = cantVotosNoValidos;
+	
+	public void setCantvotosnovalid(int cantvotosnovalid) {
+		this.cantvotosnovalid = cantvotosnovalid;
 	}
-
-
-
+	
 	@Basic(optional = false)
-	@Column(name = "UserName")
-	public String getUserName() {
-		return userName;
+	@Column(name = "username")
+	public int getUsername() {
+		return username;
 	}
-
-
-
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-
-
-
+	
+	public void setUsername(int username) {
+		this.username = username;
+	}  
+	
+		
 }
