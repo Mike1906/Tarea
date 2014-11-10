3e24abc191ff5820f9944d8661330f3d37f7f1c3
stanleyvladimir2005 <stanleyvladimir2005@hotmail.com>
2014-11-09 21:01:07 -0600

    ctrmunicipio, ctrdepartamento

--- src/negocio/CtrlDepartamento.java
+++ src/negocio/CtrlDepartamento.java
@@ -1,5 +1,52 @@
 package negocio;
+import java.util.List;
+import dao.*;
+import dominio.*;
+
+import dao.MunicipioDAO;
+import dominio.Miembrojrv;
+import dominio.Municipio;
 
 public class CtrlDepartamento {
+	private DepartamentoDAO daoMiembro = new DepartamentoDAO() ;
 
+	public boolean crearDepartamento(String nombdepto, String iddepto,String zonageografica, String username) {
+		if (daoMiembro.daMiembroByNombres(nombres) == null) {
+			Municipio miembro = new Municipio(nombdepto,iddepto,zonageografica,username);
+			daoMiembro.guardaActualiza(miembro) ;
+			return true ;
+		}
+		else
+			return false ;
+	}
+	
+	public boolean eliminarMiembro(String departamento) {
+		if (daoMiembro.daMiembroById(departamento) != null) {
+			Departamento miembro = daoMiembro.daMiembroById(departamento) ;
+			daoMiembro.eliminar(miembro) ;
+			return true ;
+		}
+		else
+			return false ;
+	}
+	public boolean actualizar(String nombdepto, String iddepto,String zonageografica, String username) { 
+	   	 if (daoMiembro.daMiembroById(iddepto) != null) {
+	           Departamento miembro = new Departamento();
+	          miembro.setiddepto(iddepto);
+	          miembro.setnombdepto(nombdepto);
+	          miembro.setzonageografica(zonageografica);
+	          miembro.setUsername(username);
+	          daoMiembro.guardaActualiza(miembro);
+	           return true ;}
+	   	 else
+	   		 return false;
+	} 
+	
+	public List<Departamento> daDepartamento(){
+		return daoMiembro.daDepartamento() ;
+	}
+	
+	public Departamento daMiembroByNombres(String nombres) {
+		return daoMiembro.daMiembroByNombres(nombres) ;
+	}
 }
--- src/negocio/CtrlMunicipio.java
+++ src/negocio/CtrlMunicipio.java
@@ -1,5 +1,49 @@
 package negocio;
+import java.util.List;
+import dao.*;
+import dominio.*;
 
 public class CtrlMunicipio {
+	private MunicipioDAO daoMiembro = new MunicipioDAO() ;
+
+	public boolean crearMunicipio(String idmunicipio, String iddepto,String nombmunicipio, String username) {
+		if (daoMiembro.daMiembroByNombres(nombres) == null) {
+			Municipio miembro = new Municipio(idmunicipio,iddepto,nombmunicipio,username);
+			daoMiembro.guardaActualiza(miembro) ;
+			return true ;
+		}
+		else
+			return false ;
+	}
+	
+	public boolean eliminarMiembro(String municipio) {
+		if (daoMiembro.daMiembroById(municipio) != null) {
+			Municipio miembro = daoMiembro.daMiembroById(municipio) ;
+			daoMiembro.eliminar(miembro) ;
+			return true ;
+		}
+		else
+			return false ;
+	}
+	public boolean actualizar(String idmunicipio, String iddepto,String nombmunicipio, String username) { 
+	   	 if (daoMiembro.daMiembroById(idmiembro) != null) {
+	           Municipio miembro = new Miembrojrv();
+	           miembro.setiddepto(iddepto);
+	           miembro.setidmunicipio(idmunicipio);
+	           miembro.setnombmunicipio(nombmunicipio);
+	           miembro.setUsername(username);
+	           daoMiembro.guardaActualiza(miembro);
+	           return true ;}
+	   	 else
+	   		 return false;
+	} 
+	
+	public List<Municipio> daMunicipio(){
+		return daoMiembro.daMunicipio() ;
+	}
+	
+	public Municipio daMiembroByNombres(String nombres) {
+		return daoMiembro.daMiembroByNombres(nombres) ;
+	}
 
 }
